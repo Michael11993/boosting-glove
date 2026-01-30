aea45a413729249dac0871c4b18489355e9ad496
Michael11993 <omorogbemichael11@gmail.com>
2026-01-30 12:37:48 +0100

    Add files via upload

---
+++ smm_reseller_frontend/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "boosting-glove-cache-v1";
+const urlsToCache = [
+  "/",
+  "/dashboard.html",
+  "/new-order.html",
+  "/orders.html",
+  "/wallet.html",
+  "/support.html",
+  "/style.css", 
+  "/script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
